2144748c19938ec63a83cc43e2bb6601859419f2
iamgonnabe <126941459+iamgonnabe@users.noreply.github.com>
2025-10-23 00:00:14 +0900

    theme

--- app/src/main/kotlin/com/pomodoro/timer/ui/theme/Color.kt
+++ app/src/main/kotlin/com/pomodoro/timer/ui/theme/Color.kt
@@ -3,82 +3,74 @@ package com.pomodoro.timer.ui.theme
 import androidx.compose.runtime.Immutable
 import androidx.compose.ui.graphics.Color
 
-internal val White = Color(0xFFFFFFFF)
-internal val Black = Color(0xFF000000)
-internal val Black100 = Color(0xFF0F0F14)
-internal val Black200 = Color(0xFF16181C)
-internal val Blue100 = Color(0xFF5FCBB2)
-internal val Grey100 = Color(0xFF292929)
-internal val Grey300 = Color(0xFFA3A3A3)
-internal val Grey400 = Color(0xFFF0F0F0)
-internal val Grey500 = Color(0xFFF6F6F6)
-internal val Red = Color(0xFFFF0000)
+val White = Color(0xFFFFFFFF)
+val Black = Color(0xFF000000)
+val Grey100 = Color(0xFFB2B2B2)
+val Grey200 = Color(0xFFCACACA)
+val Grey300 = Color(0xFFE5E5E5)
+val Grey400 = Color(0xFFEFEFF0)
+val Grey500 = Color(0xFFD2D2D4)
+
 
 @Immutable
 data class CustomColors(
-    val iconSelected: Color,
-    val iconDefault: Color,
-    val iconPrimary: Color,
-    val iconRed: Color,
-    val primary: Color,
-    val onPrimary: Color,
     val surface: Color,
-    val onSurface: Color,
-    val border: Color,
-    val borderLight : Color,
-    val textPrimary: Color,
+    val text: Color,
     val textSecondary: Color,
-    val textTertiary: Color,
-    val error: Color,
-    val buttonSurface: Color,
-    val buttonBorderUnfocused: Color,
-    val buttonBorderFocused: Color,
-    val textFieldSurface: Color,
-    val textFieldBorder: Color,
-    val placeholder: Color,
+    val buttonText: Color,
+    val buttonBorder: Color,
+    val rippledButtonText: Color,
+    val rippledButtonBorder: Color,
+    val indicatorBox: Color,
+    val dotIndicatorFocused: Color,
+    val dotIndicatorUnfocused: Color,
+    val divider: Color,
+    val textFieldContainer: Color,
+    val textFieldText: Color,
+    val selectorTextSelected: Color,
+    val selectorTextUnselected: Color,
+    val selectorDivider: Color,
+    val selectorContainer: Color,
+    val selectorContainerSelected: Color,
 )
 
 val LightCustomColors = CustomColors(
-    iconSelected = Black,
-    iconDefault = Grey300,
-    iconPrimary = Blue100,
-    iconRed = Red,
-    primary = Blue100,
-    onPrimary = White,
-    surface = Grey500,
-    onSurface = White,
-    border = Grey300,
-    borderLight = Grey400,
-    textPrimary = Black,
-    textSecondary = Grey300,
-    textTertiary = Grey400,
-    error = Red,
-    buttonSurface = White,
-    buttonBorderUnfocused = Grey400,
-    buttonBorderFocused = Black,
-    textFieldSurface = White,
-    textFieldBorder = Black,
-    placeholder = Grey300,
+    surface = White,
+    text = Black,
+    textSecondary = White,
+    buttonText = Black,
+    buttonBorder = Black,
+    rippledButtonText = Black,
+    rippledButtonBorder = Black,
+    indicatorBox = Grey200,
+    dotIndicatorFocused = Black,
+    dotIndicatorUnfocused = Grey100,
+    divider = Grey300,
+    textFieldContainer = Grey400,
+    textFieldText = Black,
+    selectorTextSelected = Black,
+    selectorTextUnselected = Black,
+    selectorDivider = Grey500,
+    selectorContainer = Grey400,
+    selectorContainerSelected = White,
 )
 val DarkCustomColors = CustomColors(
-    iconSelected = White,
-    iconDefault = Grey300,
-    iconPrimary = Blue100,
-    iconRed = Red,
-    primary = Blue100,
-    onPrimary = White,
-    surface = Black100,
-    onSurface = Black200,
-    border = Grey300,
-    borderLight = Grey100,
-    textPrimary = White,
-    textSecondary = Grey300,
-    textTertiary = Grey400,
-    error = Red,
-    buttonSurface = Black,
-    buttonBorderUnfocused = Grey300,
-    buttonBorderFocused = White,
-    textFieldSurface = Black200,
-    textFieldBorder = White,
-    placeholder = Grey300,
+    surface = Black,
+    text = White,
+    textSecondary = White,
+    buttonText = White,
+    buttonBorder = White,
+    rippledButtonText = White,
+    rippledButtonBorder = White,
+    indicatorBox = White,
+    dotIndicatorFocused = White,
+    dotIndicatorUnfocused = Grey100,
+    divider = Grey300,
+    textFieldContainer = Grey400,
+    textFieldText = Black,
+    selectorTextSelected = Black,
+    selectorTextUnselected = Black,
+    selectorDivider = Grey500,
+    selectorContainer = Grey400,
+    selectorContainerSelected = Black
 )
--- app/src/main/kotlin/com/pomodoro/timer/ui/theme/Typo.kt
+++ app/src/main/kotlin/com/pomodoro/timer/ui/theme/Typo.kt
@@ -10,18 +10,22 @@ import com.pomodoro.timer.R
 
 @Immutable
 data class CustomTypography(
-    val headline1: TextStyle,
-    val headline2: TextStyle,
-    val headline3: TextStyle,
-    val title1: TextStyle,
-    val title2: TextStyle,
-    val body1: TextStyle,
-    val body2: TextStyle,
-    val body3: TextStyle,
-    val button1: TextStyle,
-    val button2: TextStyle,
-    val caption1: TextStyle,
-    val caption2: TextStyle
+    val buttonTimerSmall: TextStyle,
+    val buttonTimerLarge: TextStyle,
+    val buttonEditSmall: TextStyle,
+    val buttonEditLarge: TextStyle,
+    val clockTimeSmall: TextStyle,
+    val clockTimeLarge: TextStyle,
+    val digitalTimeSmall: TextStyle,
+    val digitalTimeLarge: TextStyle,
+    val deskTimeSmall: TextStyle,
+    val deskTimeLarge: TextStyle,
+    val editTitleSmall: TextStyle,
+    val editTitleLarge: TextStyle,
+    val selectorSelected: TextStyle,
+    val selectorUnSelected: TextStyle,
+    val textField: TextStyle,
+    val textPreview: TextStyle
 )
 
 val fontFamily = FontFamily(
@@ -35,18 +39,25 @@ val fontFamily = FontFamily(
     Font(R.font.pretendard_thin, FontWeight.Thin),
     Font(R.font.pretendard_extralight, FontWeight.ExtraLight),
 )
+val digitalMonoFont = FontFamily(
+    Font(R.font.digital_7_mono),
+)
 
 val customTypography =  CustomTypography(
-    headline1 = TextStyle(fontFamily = fontFamily, fontWeight = FontWeight.ExtraBold, fontSize = 32.sp),
-    headline2 = TextStyle(fontFamily = fontFamily, fontWeight = FontWeight.ExtraBold, fontSize = 24.sp),
-    headline3 = TextStyle(fontFamily = fontFamily, fontWeight = FontWeight.ExtraBold, fontSize = 20.sp),
-    title1 = TextStyle(fontFamily = fontFamily, fontWeight = FontWeight.Bold, fontSize = 16.sp),
-    title2 = TextStyle(fontFamily = fontFamily, fontWeight = FontWeight.Bold, fontSize = 14.sp),
-    body1 = TextStyle(fontFamily = fontFamily, fontWeight = FontWeight.Normal, fontSize = 16.sp),
-    body2 = TextStyle(fontFamily = fontFamily, fontWeight = FontWeight.Normal, fontSize = 14.sp),
-    body3 = TextStyle(fontFamily = fontFamily, fontWeight = FontWeight.Normal, fontSize = 12.sp),
-    button1 = TextStyle(fontFamily = fontFamily, fontWeight = FontWeight.Light, fontSize = 30.sp),
-    button2 = TextStyle(fontFamily = fontFamily, fontWeight = FontWeight.Medium, fontSize = 13.sp),
-    caption1 = TextStyle(fontFamily = fontFamily, fontWeight = FontWeight.Normal, fontSize = 12.sp),
-    caption2 = TextStyle(fontFamily = fontFamily, fontWeight = FontWeight.Normal, fontSize = 10.sp)
+    buttonTimerSmall = TextStyle(fontFamily = fontFamily, fontWeight = FontWeight.Light, fontSize = 30.sp),
+    buttonTimerLarge = TextStyle(fontFamily = fontFamily, fontWeight = FontWeight.Light, fontSize = 40.sp),
+    buttonEditSmall = TextStyle(fontFamily = fontFamily, fontWeight = FontWeight.SemiBold, fontSize = 14.sp),
+    buttonEditLarge = TextStyle(fontFamily = fontFamily, fontWeight = FontWeight.SemiBold, fontSize = 14.sp),
+    clockTimeSmall = TextStyle(fontFamily = fontFamily, fontWeight = FontWeight.Normal, fontSize = 37.sp),
+    clockTimeLarge = TextStyle(fontFamily = fontFamily, fontWeight = FontWeight.Normal, fontSize = 82.sp),
+    digitalTimeSmall = TextStyle(fontFamily = fontFamily, fontWeight = FontWeight.Normal, fontSize = 60.sp),
+    digitalTimeLarge = TextStyle(fontFamily = fontFamily, fontWeight = FontWeight.Normal, fontSize = 100.sp),
+    deskTimeSmall = TextStyle(fontFamily = digitalMonoFont, fontSize = 76.sp),
+    deskTimeLarge = TextStyle(fontFamily = digitalMonoFont, fontSize = 300.sp),
+    editTitleSmall = TextStyle(fontFamily = fontFamily, fontWeight = FontWeight.Bold, fontSize = 18.sp),
+    editTitleLarge = TextStyle(fontFamily = fontFamily, fontWeight = FontWeight.Bold, fontSize = 50.sp),
+    selectorSelected = TextStyle(fontFamily = fontFamily, fontWeight = FontWeight.SemiBold, fontSize = 13.sp),
+    selectorUnSelected = TextStyle(fontFamily = fontFamily, fontWeight = FontWeight.Normal, fontSize = 13.sp),
+    textField = TextStyle(fontFamily = fontFamily, fontWeight = FontWeight.Normal, fontSize = 13.sp),
+    textPreview = TextStyle(fontFamily = fontFamily, fontWeight = FontWeight.SemiBold, fontSize = 50.sp)
 )
