6239f597ee4b477c1d1553c2e42ed1cbf6700612
iamgonnabe <126941459+iamgonnabe@users.noreply.github.com>
2025-10-25 23:36:21 +0900

    리플 효과 x

--- app/src/main/kotlin/com/pomodoro/timer/presentation/common/TimerEditBox.kt
+++ app/src/main/kotlin/com/pomodoro/timer/presentation/common/TimerEditBox.kt
@@ -3,6 +3,7 @@ package com.pomodoro.timer.presentation.common
 import android.annotation.SuppressLint
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -22,6 +23,7 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.VerticalDivider
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
@@ -165,7 +167,7 @@ fun GapBox(
     onClickGap: (Int) -> Unit,
 ){
     Column(
-        modifier = Modifier.fillMaxWidth(),
+        modifier = Modifier.fillMaxWidth(0.8f),
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.spacedBy(12.dp)
     ) {
@@ -188,7 +190,9 @@ fun GapBox(
                         color = if(gap == i*5) CustomTheme.colors.selectorContainerSelected else Color.Transparent,
                         shape = RoundedCornerShape(12.dp)
                     ).weight(1f).padding(vertical = 4.dp).clickable(
-                        onClick = { onClickGap(i*5) }
+                        onClick = { onClickGap(i*5) },
+                        interactionSource = remember { MutableInteractionSource() },
+                        indication = null
                     ),
                     contentAlignment = Alignment.Center
                 ){
