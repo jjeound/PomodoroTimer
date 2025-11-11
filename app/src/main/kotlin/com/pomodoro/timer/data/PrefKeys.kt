496eb136a832d318d316dbc03c912b8f22e534a5
iamgonnabe <126941459+iamgonnabe@users.noreply.github.com>
2025-11-11 19:55:04 +0900

    first launch

--- app/src/main/kotlin/com/pomodoro/timer/data/PrefKeys.kt
+++ app/src/main/kotlin/com/pomodoro/timer/data/PrefKeys.kt
@@ -1,7 +1,9 @@
 package com.pomodoro.timer.data
 
+import androidx.datastore.preferences.core.booleanPreferencesKey
 import androidx.datastore.preferences.core.longPreferencesKey
 
 object PrefKeys {
     val WIDGET_ID = longPreferencesKey("widget_id")
+    val IS_FIRST_ENTER = booleanPreferencesKey("is_first_enter")
 }
